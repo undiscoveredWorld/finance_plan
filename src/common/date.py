5368c8d29c19edd489977333f6697d34bd7207c4
undiscoveredWorld <newspaw1111@mail.ru>
2024-04-08 18:11:11 +0000

    Created firstly models for primary models

---
+++ src/common/date.py
@@ -0,0 +1,12 @@
+class Date(object):
+    day: int
+    month: int
+    year: int
+
+    def __init__(self, day: int, month: int, year: int) -> None:
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __str__(self) -> str:
+        return f"{self.month}/{self.day}/{self.year}"
---
+++ src/domain/models.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+
+from src.common.date import Date
+
+
+class CategoryBase(BaseModel):
+    name: str
+
+
+class CategoryCreate(CategoryBase):
+    ...
+
+
+class Category(CategoryBase):
+    id: int
+
+
+class SubCategoryBase(BaseModel):
+    name: str
+
+
+class SubCategoryCreate(SubCategoryBase):
+    ...
+
+
+class SubCategory(SubCategoryBase):
+    id: int
+
+
+class ProductBase(BaseModel):
+    name: str
+
+
+class ProductCreate(ProductBase):
+    ...
+
+
+class Product(ProductBase):
+    id: int
+
+
+class BuyBase(BaseModel):
+    date: Date
+    category: CategoryBase
+    subcategory: SubCategoryBase
+    product: ProductBase
+
+
+class BuyCreate(BuyBase):
+    ...
+
+
+class Buy(BuyBase):
+    id: int
